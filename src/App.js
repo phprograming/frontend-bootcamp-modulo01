f7a1b57e8222ca3e6aad69efc00489118357c090
phprograming <paulo_hbs17@hotmail.com>
2020-10-30 13:47:18 -0300

    Placing properties

--- src/App.js
+++ src/App.js
@@ -2,10 +2,28 @@ import React from 'react';
 
 import Header from './components/Header';
 
-export default function App() {
-    return (
+function App() {
+    const projects = ['Desenvolvimento de app', 'Front-end web'];
+    
+    function handleAddProject() {
+        projects.push(`Novo projeto ${Date.now()}`);
+
+        console.log(projects);
+    }
+
+    return ( 
         <>
-            <Header />
+            <Header title="Homepage" />
+            
+            <ul>
+                {projects.map(project => {
+                    <li key={project}>{project}</li>
+                })}
+            </ul>
+            
+            <button type="button" onClick={ handleAddProject }>Adicionar projeto</button>         
         </>
     );
 }
+
+export default App;
