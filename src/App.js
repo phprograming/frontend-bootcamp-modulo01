1c15fac107a13e3ce560d92921b7f7f8248ce02b
Paulo Henrique Barbosa Santos <paulo_hbs17@hotmail.com>
2020-10-30 22:04:38 -0300

    Finish frontend

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+
+import api from './services/api';
+
+import './App.css';
 
 import Header from './components/Header';
 
 function App() {
-    const projects = ['Desenvolvimento de app', 'Front-end web'];
-    
-    function handleAddProject() {
-        projects.push(`Novo projeto ${Date.now()}`);
+  const [projects, setProjects] = useState([]);
 
-        console.log(projects);
-    }
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    })
+  }, []);
+  
+  async function handleAddProject() {
 
-    return ( 
-        <>
-            <Header title="Homepage" />
-            
-            <ul>
-                {projects.map(project => {
-                    <li key={project}>{project}</li>
-                })}
-            </ul>
-            
-            <button type="button" onClick={ handleAddProject }>Adicionar projeto</button>         
-        </>
-    );
+    const response = await api.post('projects', {
+      title: `Novo Projeto ${Date.now()}`,
+      owner: 'Danilo Vieira'
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
+
+  return (
+    <>
+      <Header title="Projects" />
+
+      <ul>
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}></button>
+    </>
+  );
 }
 
 export default App;
